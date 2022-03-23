a0deb36c19fa0a462b312b6127ae1a2eda95254a
Somodi Konrád <somodikonrad@turr.hu>
2022-03-23 11:21:44 +0100

    foablak icon cserélése

--- henger.py
+++ henger.py
@@ -1,6 +1,15 @@
 from tkinter import*
 import math
 
+gyoker = 'D:\\IKT Konrád\\IKT-pythonSK\\'
+
+
+
+def szamitas():
+    r = int(sugarb.get())
+    m = int(magasg.get())
+    terfogat = math.pi * r * r * m
+    
 foablak = Tk()
 
 
@@ -17,7 +26,7 @@ magasg.grid(column = 2, row = 2, columnspan = 2)
 gomb1 = Button(foablak, text = 'kiszámítás')
 gomb1.grid(column = 3, row = 3)
 
-terfogat = Label(foablak, text= 'Térfogat(cm3):')
+terfogat = Label(foablak, text= 'Térfogat(cm³):')
 terfogat.grid(column = 1, row = 4, columnspan = 1)
 terfb = Entry(foablak)
 terfb.grid(column = 2, row = 4, columnspan = 2)
@@ -27,4 +36,12 @@ Vashenger.grid(column = 1, row = 5)
 vashengb = Entry(foablak)
 vashengb.grid(column = 2, row = 5, columnspan = 2)
 
+fahenger = Label(foablak, text= 'Fahenger(cm):')
+fahenger.grid(column = 1, row = 6)
+fahengb = Entry(foablak)
+fahengb.grid(column = 2, row = 6, columnspan = 2)
+
+
+photo = PhotoImage(file = gyoker + 'shiba.gif')
+foablak.iconphoto(True, photo)
 foablak.mainloop()
