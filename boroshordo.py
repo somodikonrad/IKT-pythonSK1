12f3ca722abe1d5874dbf8373513ebf0fdb4fa91
somodikonrad <78954961+somodikonrad@users.noreply.github.com>
2022-03-29 11:34:23 +0200

    kep hozzaadas

--- boroshordo.py
+++ boroshordo.py
@@ -1,8 +1,13 @@
 from tkinter import*
-import math
+
 
 foablak = Tk()
 
+
+foablak.geometry('550x550')
+foablak.title('Boroshordó')
+vaszon=Canvas(foablak, width=160, height=160, bg='white')
+kep = PhotoImage(file = 'D:\\IKT Konrád\\IKT-pythonSK\\hordo.png')
 
 mennyiseg = Label(foablak, text= 'Bor mennyisége(l):')
 mennyiseg.grid(column = 1, row = 1)
@@ -21,8 +26,5 @@ magasg.grid(column = 2, row = 3, columnspan = 2)
 
 gomb1 = Button(foablak, text = 'kiszámítás')
 gomb1.grid(column = 3, row = 4)
-
-
-
 
 foablak.mainloop()
